sthagen/schemathesis-schemathesis

chore: Ugly prototype

src/schemathesis/service/report.py
@@ -1,31 +1,32 @@
 import json
 import tarfile
+import threading
 import time
 from io import BytesIO
 from queue import Queue
-from typing import Any
+from typing import Any, Optional
 
 import attr
 
 from ..cli.context import ExecutionContext
 from ..cli.handlers import EventHandler
-from ..runner.events import ExecutionEvent, Finished
+from ..runner.events import ExecutionEvent
 from . import ServiceClient, events
+from .constants import STOP_MARKER, WORKER_JOIN_TIMEOUT
 from .metadata import Metadata
+from .models import AnonymousUploadResponse
 from .serialization import serialize_event
 
 
 @attr.s(slots=True)
-class Report:
-    """Schemathesis.io test run report."""
+class ReportWriter:
+    """Schemathesis.io test run report.
 
-    _data: BytesIO = attr.ib(factory=BytesIO)
-    _events_count: int = attr.ib(default=1)
-    _tar: tarfile.TarFile = attr.ib(init=False)
+    Simplifies adding new files to the archive.
+    """
 
-    def __attrs_post_init__(self) -> None:
-        # pylint: disable=consider-using-with
-        self._tar = tarfile.open(mode="w:gz", fileobj=self._data)
+    _tar: tarfile.TarFile = attr.ib()
+    _events_count: int = attr.ib(default=0)
 
     def _add_json_file(self, name: str, data: Any) -> None:
         buffer = BytesIO()
@@ -36,33 +37,75 @@ class Report:
         info.mtime = int(time.time())
         self._tar.addfile(info, buffer)
 
-    def add_metadata(self, metadata: Metadata) -> None:
-        self._add_json_file("metadata.json", attr.asdict(metadata))
+    def add_metadata(self, slug: Optional[str], metadata: Metadata) -> None:
+        data = {
+            # API identifier on the Schemathesis.io side (optional)
+            "slug": slug,
+            ""
+            # Metadata about CLI environment
+            "environment": attr.asdict(metadata),
+        }
+        self._add_json_file("metadata.json", data)
 
     def add_event(self, event: ExecutionEvent) -> None:
         """Add an execution event to the report."""
-        self._add_json_file(f"events/{self._events_count}.json", serialize_event(event))
         self._events_count += 1
-
-    def finish(self) -> bytes:
-        """Finish the report and get the underlying data."""
-        self._tar.close()
-        return self._data.getvalue()
+        self._add_json_file(f"events/{self._events_count}.json", serialize_event(event))
 
 
 @attr.s(slots=True)  # pragma: no mutate
 class ReportHandler(EventHandler):
     client: ServiceClient = attr.ib()  # pragma: no mutate
+    api_slug: Optional[str] = attr.ib()  # pragma: no mutate
     out_queue: Queue = attr.ib()  # pragma: no mutate
-    report: Report = attr.ib(factory=Report)  # pragma: no mutate
+    in_queue: Queue = attr.ib(factory=Queue)  # pragma: no mutate
+    worker: threading.Thread = attr.ib(init=False)  # pragma: no mutate
+
+    def __attrs_post_init__(self) -> None:
+        self.worker = threading.Thread(
+            target=start,
+            kwargs={
+                "client": self.client,
+                "api_slug": self.api_slug,
+                "in_queue": self.in_queue,
+                "out_queue": self.out_queue,
+            },
+        )
+        self.worker.start()
 
     def handle_event(self, context: ExecutionContext, event: ExecutionEvent) -> None:
-        self.report.add_event(event)
-        if isinstance(event, Finished):
-            try:
-                self.report.add_metadata(Metadata())
-                payload = self.report.finish()
-                self.client.upload_report(payload)
-                self.out_queue.put(events.Completed(short_url="TODO"))
-            except Exception as exc:
-                self.out_queue.put(events.Error(exc))
+        self.in_queue.put(event)
+
+    def shutdown(self) -> None:
+        self._stop_worker()
+
+    def _stop_worker(self) -> None:
+        self.in_queue.put(STOP_MARKER)
+        self.worker.join(WORKER_JOIN_TIMEOUT)
+
+
+def start(client: ServiceClient, api_slug: Optional[str], in_queue: Queue, out_queue: Queue) -> None:
+    """Create a compressed ``tar.gz`` file during the run & upload it to Schemathesis.io when the run is finished."""
+    payload = BytesIO()
+    try:
+        with tarfile.open(mode="w:gz", fileobj=payload) as tar:
+            writer = ReportWriter(tar)
+            writer.add_metadata(api_slug, Metadata())
+            while True:
+                event = in_queue.get()
+                if event is STOP_MARKER:
+                    # Happens only if an exception happened in another thread
+                    break
+                # Add every event to the report
+                writer.add_event(event)
+                if event.is_terminal:
+                    break
+        response = client.upload_report(payload.getvalue())
+        if isinstance(response, AnonymousUploadResponse):
+            event = events.SuccessfulAnonymousUpload(message=response.message, signup_url=response.signup_url)
+        else:
+            event = events.SuccessfulUpload(message=response.message, report_url=response.report_url)
+        out_queue.put(event)
+        # TODO. do not upload test runs that did not start properly / interrupted ones
+    except Exception as exc:
+        out_queue.put(events.Error(exc))

frederikaalund/schemathesis

src/schemathesis/service/report.py
@@ -1,33 +1,33 @@
 import json
 import tarfile
+import threading
 import time
 from io import BytesIO
 from queue import Queue
-from typing import Any
+from typing import Any, Optional
 
 import attr
 
 from ..cli.context import ExecutionContext
 from ..cli.handlers import EventHandler
-from ..runner.events import ExecutionEvent, Finished
+from ..runner.events import ExecutionEvent, Initialized
 from . import ServiceClient, events
+from .constants import REPORT_VERSION, STOP_MARKER, WORKER_JOIN_TIMEOUT
 from .metadata import Metadata
 from .serialization import serialize_event
 
 
 @attr.s(slots=True)
-class Report:
-    """Schemathesis.io test run report."""
+class ReportWriter:
+    """Schemathesis.io test run report.
 
-    _data: BytesIO = attr.ib(factory=BytesIO)
-    _events_count: int = attr.ib(default=1)
-    _tar: tarfile.TarFile = attr.ib(init=False)
+    Simplifies adding new files to the archive.
+    """
 
-    def __attrs_post_init__(self) -> None:
-        # pylint: disable=consider-using-with
-        self._tar = tarfile.open(mode="w:gz", fileobj=self._data)
+    _tar: tarfile.TarFile = attr.ib()
+    _events_count: int = attr.ib(default=0)
 
-    def _add_json_file(self, name: str, data: Any) -> None:
+    def add_json_file(self, name: str, data: Any) -> None:
         buffer = BytesIO()
         buffer.write(json.dumps(data, separators=(",", ":")).encode())
         buffer.seek(0)
@@ -36,33 +36,85 @@ class Report:
         info.mtime = int(time.time())
         self._tar.addfile(info, buffer)
 
-    def add_metadata(self, metadata: Metadata) -> None:
-        self._add_json_file("metadata.json", attr.asdict(metadata))
+    def add_metadata(self, *, api_name: Optional[str], location: str, base_url: str, metadata: Metadata) -> None:
+        data = {
+            # API identifier on the Schemathesis.io side (optional)
+            "api_name": api_name,
+            # The place, where the API schema is located
+            "location": location,
+            # The base URL against which the tests are running
+            "base_url": base_url,
+            # Metadata about CLI environment
+            "environment": attr.asdict(metadata),
+            "version": REPORT_VERSION,
+        }
+        self.add_json_file("metadata.json", data)
 
     def add_event(self, event: ExecutionEvent) -> None:
         """Add an execution event to the report."""
-        self._add_json_file(f"events/{self._events_count}.json", serialize_event(event))
         self._events_count += 1
-
-    def finish(self) -> bytes:
-        """Finish the report and get the underlying data."""
-        self._tar.close()
-        return self._data.getvalue()
+        filename = f"events/{self._events_count}-{event.__class__.__name__}.json"
+        self.add_json_file(filename, serialize_event(event))
 
 
 @attr.s(slots=True)  # pragma: no mutate
 class ReportHandler(EventHandler):
     client: ServiceClient = attr.ib()  # pragma: no mutate
+    api_name: Optional[str] = attr.ib()  # pragma: no mutate
+    location: str = attr.ib()  # pragma: no mutate
+    base_url: Optional[str] = attr.ib()  # pragma: no mutate
     out_queue: Queue = attr.ib()  # pragma: no mutate
-    report: Report = attr.ib(factory=Report)  # pragma: no mutate
+    in_queue: Queue = attr.ib(factory=Queue)  # pragma: no mutate
+    worker: threading.Thread = attr.ib(init=False)  # pragma: no mutate
+
+    def __attrs_post_init__(self) -> None:
+        self.worker = threading.Thread(
+            target=start,
+            kwargs={
+                "client": self.client,
+                "api_name": self.api_name,
+                "location": self.location,
+                "base_url": self.base_url,
+                "in_queue": self.in_queue,
+                "out_queue": self.out_queue,
+            },
+        )
+        self.worker.start()
 
     def handle_event(self, context: ExecutionContext, event: ExecutionEvent) -> None:
-        self.report.add_event(event)
-        if isinstance(event, Finished):
-            try:
-                self.report.add_metadata(Metadata())
-                payload = self.report.finish()
-                self.client.upload_report(payload)
-                self.out_queue.put(events.Completed(short_url="TODO"))
-            except Exception as exc:
-                self.out_queue.put(events.Error(exc))
+        self.in_queue.put(event)
+
+    def shutdown(self) -> None:
+        self._stop_worker()
+
+    def _stop_worker(self) -> None:
+        self.in_queue.put(STOP_MARKER)
+        self.worker.join(WORKER_JOIN_TIMEOUT)
+
+
+def start(
+    client: ServiceClient, api_name: Optional[str], location: str, base_url: str, in_queue: Queue, out_queue: Queue
+) -> None:
+    """Create a compressed ``tar.gz`` file during the run & upload it to Schemathesis.io when the run is finished."""
+    payload = BytesIO()
+    try:
+        with tarfile.open(mode="w:gz", fileobj=payload) as tar:
+            writer = ReportWriter(tar)
+            writer.add_metadata(api_name=api_name, location=location, base_url=base_url, metadata=Metadata())
+            while True:
+                event = in_queue.get()
+                if event is STOP_MARKER:
+                    # Happens only if an exception happened in another thread
+                    break
+                # Add every event to the report
+                if isinstance(event, Initialized):
+                    writer.add_json_file("schema.json", event.schema)
+                writer.add_event(event)
+                if event.is_terminal:
+                    break
+        response = client.upload_report(payload.getvalue())
+        event = events.SuccessfulUpload(message=response.message, next_url=response.next_url)
+        out_queue.put(event)
+        # TODO. do not upload test runs that did not start properly / interrupted ones
+    except Exception as exc:
+        out_queue.put(events.Error(exc))